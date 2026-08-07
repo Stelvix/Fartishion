3e9cd7f5f18fece37c1a37ca9c109e25736c8159
riedsteeve <145045537+riedsteeve@users.noreply.github.com>
2026-08-07 17:14:41 +0200

    Modif de la CI

--- backend/Services/UserService.cs
+++ backend/Services/UserService.cs
@@ -3,7 +3,6 @@ using backend.DTO;
 using Microsoft.EntityFrameworkCore;
 using BCrypt.Net;
 
-
 namespace backend.Services
 {
     public class UserService(PostgresContext context, IHttpContextAccessor httpContextAccessor) : IUserService
